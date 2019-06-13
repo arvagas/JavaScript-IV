579b43d7589120ebaa8b52194b44765ee76c0379
Arvin Agas <arvagas@gmail.com>
2019-06-13 16:33:18 -0400

    finished Student and Project Manager class extensions Imported example constants from slack

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -24,3 +24,149 @@ class Instructor extends Person {
         return `${student.name} receives a perfect score on ${subject}`
     }
 }
+
+class Student extends Person {
+    constructor(attributes) {
+        super(attributes)
+        this.previousBackground = attributes.previousBackground
+        this.className = attributes.className
+        this.favSubjects = attributes.favSubjects
+    }
+    listsSubjects() {
+        console.log(...favSubjects)
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(attributes) {
+        super(attributes)
+        this.gradClassName = attributes.gradClassName
+        this.favInstructor = attributes.favInstructor
+    }
+    standUp(slack) {
+        return `${this.name} announces to ${slack}, @channel standy times!`
+    }
+    debugsCode(student,subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
+}
+
+const dan = new Instructor({
+    name: 'Dan',
+    age: 'Infinity',
+    location: 'Denver',
+    specialty: 'Relentless Debugger',
+    favLanguage: 'JavaScript, Python, Elm, ReasonML, SmallTalk, Haskell, C#, Java, Rust, Go, Ruby, Crystal, Elixir, Lua, and Julia',
+    catchPhrase: 'If you can do the thing, you can get paid to do the thing!',
+});
+
+const isaiah = new Student({
+    name: 'Isaiah',
+    age: 18,
+    location: 'Florida',
+    previousBackground: 'High School last month',
+    className: 'Web21',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+const kevin = new Student({
+    name: "Kevin",
+    age: 28,
+    location: "California",
+    previousBackground: "Table Games Dealer",
+    className: "WEB21",
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+const nisa = new Student({
+    name: 'Nisa',
+    age: 25,
+    location: 'Ohio',
+    previousBackground: 'Debt Collector',
+    className: 'Web21',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+const joscelyn = new Student({
+    name: "Joscelyn",
+    age: 29,
+    location: "California",
+    previousBackground: "English teacher",
+    className: 'Web21',
+    favSubjects: ["Computer Science", "Philosophy", "English"],
+});
+
+const marguel = new ProjectManager({
+    name: 'Marguel',
+    age: 'Maybe 26',
+    gradClassName: 'WEBPT2',
+    favInstructor: 'Me?',
+    location: 'California',
+    specialty: 'React',
+    favLanguage: 'JavaScript, Python, Elm etc.',
+    catchPhrase: "Practice Flex Zombies !!!",
+});
+const brandon = new ProjectManager({
+    name: 'Brandon',
+    age: '34',
+    gradClassName: 'WEB18',
+    favInstructor: 'Professor Lambda',
+    location: 'Maine',
+    specialty: 'Redux',
+    favLanguage: 'JavaScript, C++, Python.',
+    catchPhrase: "You shall not pass!",
+});
+const mary = new ProjectManager({
+    name: 'Mary',
+    age: '24',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'New York',
+    specialty: 'Express and Node.js',
+    favLanguage: 'Javascript',
+    catchPhrase: "That looks AWESOME",
+});
+const christian = new ProjectManager({
+    name: 'Christian',
+    age: '32',
+    gradClassName: 'WEB18',
+    favInstructor: 'Every Instructor in Lambda',
+    location: 'Seattle, WA',
+    specialty: 'Data Structures & Algorithms',
+    favLanguage: 'JavaScript',
+    catchPhrase: "Dont forget your daily commit.",
+});
+const pat = new ProjectManager({
+    name: 'Pat',
+    age: '38',
+    gradClassName: 'WEB18',
+    favInstructor: 'Brett Madrid',
+    location: 'Petaluma, Ca',
+    specialty: 'Empathetic to the struggle of Redux',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'Lets google that together.'
+});
+const darren = new ProjectManager({
+    name: 'Darren',
+    age: '25',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'North Carolina',
+    specialty: 'React',
+    favLanguage: 'Javascript',
+    catchPhrase: 'Gang. Gang.',
+});
+const austin = new ProjectManager({
+    name: 'Austin',
+    age: '23',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'Somewhere',
+    specialty: 'Java',
+    favLanguage: 'Java',
+    catchPhrase: ":eggplant:",
+});
+
