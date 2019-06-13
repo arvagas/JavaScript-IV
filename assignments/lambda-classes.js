0d3aa5c9fef1074ef082ac3fdfa0a8848e1989ac
Arvin Agas <arvagas@gmail.com>
2019-06-13 16:21:06 -0400

    initalized base Person class, Instructor extension

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,26 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name
+        this.age = attributes.age
+        this.location = attributes.location
+    }
+    speak () {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person {
+    constructor(attributes) {
+        super(attributes)
+        this.specialty = this.specialty
+        this.favLanguage = attributes.favLanguage
+        this.catchPhrase = attributes.catchPhrase
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`
+    }
+    grade(student,subject) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
